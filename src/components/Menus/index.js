40755d5d6a4187eae4b6cb254f3411e70f72d940
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-07-11 23:43:18 +0700

    fix design card menus component

--- src/components/Menus/index.js
+++ src/components/Menus/index.js
@@ -6,9 +6,9 @@ const Menus = ({menu}) => {
   return (
     <Col md={4} xs={6} className="mb-4">
       <Card className="shadow">
-        <Card.Img variant="top" src={`assets/images/${menu.category.name.toLowerCase()}/${menu.image}`} />
+        <Card.Img variant="top" src={`assets/images/${menu.category.name.toLowerCase()}/${menu.image}`} style={{width: '100%', height: 200}} />
         <Card.Body>
-          <Card.Title>{menu.name}</Card.Title>
+          <Card.Title as="h6" as="strong">{menu.name}</Card.Title>
           <Card.Text>
             Rp. {numberWithCommas(menu.price)}
           </Card.Text>
