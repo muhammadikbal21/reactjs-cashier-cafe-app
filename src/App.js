b25d4815279724a7837cfa54c9af525d2b5860b4
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-07-11 22:39:52 +0700

    hit API with JSON Server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import React, { Component } from 'react'
 import { Container, Row } from 'react-bootstrap';
 import { Header, Products, Categories, Cart } from './components';
+import { API_URL } from './utils/constants'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <Categories />
-            <Products />
-            <Cart />
-          </Row>
-        </Container>
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      menus: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(API_URL+"products")
+    .then(res => {
+      console.log("Response: ", res);
+      const menus = res.data
+      this.setState({menus: menus})
+    })
+    .catch(err => {
+      console.log("Ini Error Ya: ", err);
+    })
+  }
+  
+  render() {
+    console.log("ini data di render: ", this.state.menus);
+    return (
+      <div className="App">
+        <Header />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <Categories />
+              <Products />
+              <Cart />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
-
-export default App;
